00cbfa701c65e5b80701fe10f46a32b261405fc8
Gonnot <vianney.gonnot@efrei.net>
2021-03-20 17:06:24 +0100

    graph

---
+++ app.py
@@ -0,0 +1,39 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.graph_objects as go
+import pandas as pd
+from dash.dependencies import Input, Output
+
+# Load data
+df = pd.read_csv('database.csv')
+df
+#df.index = pd.to_datetime(df['Date'])
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Crimes Analytics",),
+        html.P(
+            children="Analyse crimes by year",
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": df["Year"],
+                        "y": df["Incident"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Incident per year"},
+            },
+        ),
+
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
