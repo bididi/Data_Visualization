738d11f9c32ed337ae33f1996bc6f67358f8edd2
Elodie <elodie.gueuret@efrei.net>
2021-03-23 19:00:09 +0100

    Avancement du css

--- app.py
+++ app.py
@@ -11,76 +11,75 @@ import plotly.express as px
 # Load data
 
 df = pd.read_csv('database.csv')
-#df=df[:100000]
+# df=df[:100000]
 df.dropna(inplace=True)
 # print(pd.isnull(df).sum())
 # df.index = pd.to_datetime(df['Date'])
 nbr_crime = df['Record_ID'].count()
-#print(nbr_crime)
+# print(nbr_crime)
 unsolved = len(df[df["Crime_Solved"] != "Yes"])
 solved = len(df[df["Crime_Solved"] == "Yes"])
-Total = unsolved+solved
-Mean = (solved/Total)*100
+Total = unsolved + solved
+Mean = (solved / Total) * 100
 print(Mean)
-Mean = round(Mean,2)
+Mean = round(Mean, 2)
 Weapon = df["Weapon"].value_counts().idxmax()
-#print(Weapon)
-Mean =str(Mean)
+# print(Weapon)
+Mean = str(Mean)
 
 df[["State"]] = df[["State"]].replace({'Alabama': 'AL',
-    'Alaska': 'AK',
-    'Arizona': 'AZ',
-    'Arkansas': 'AR',
-    'California': 'CA',
-    'Colorado': 'CO',
-    'Connecticut': 'CT',
-    'Delaware': 'DE',
-    'District of Columbia': 'DC',
-    'Florida': 'FL',
-    'Georgia': 'GA',
-    'Hawaii': 'HI',
-    'Idaho': 'ID',
-    'Illinois': 'IL',
-    'Indiana': 'IN',
-    'Iowa': 'IA',
-    'Kansas': 'KS',
-    'Kentucky': 'KY',
-    'Louisiana': 'LA',
-    'Maine': 'ME',
-    'Maryland': 'MD',
-    'Massachusetts': 'MA',
-    'Michigan': 'MI',
-    'Minnesota': 'MN',
-    'Mississippi': 'MS',
-    'Missouri': 'MO',
-    'Montana': 'MT',
-    'Nebraska': 'NE',
-    'Nevada': 'NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
-    'Ohio': 'OH',
-    'Oklahoma': 'OK',
-    'Oregon': 'OR',
-    'Pennsylvania': 'PA',
-    'Rhodes Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
-    'Tennessee': 'TN',
-    'Texas': 'TX',
-    'Utah': 'UT',
-    'Vermont': 'VT',
-    'Virginia': 'VA',
-    'Washington': 'WA',
-    'West Virginia': 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY'})
+                                       'Alaska': 'AK',
+                                       'Arizona': 'AZ',
+                                       'Arkansas': 'AR',
+                                       'California': 'CA',
+                                       'Colorado': 'CO',
+                                       'Connecticut': 'CT',
+                                       'Delaware': 'DE',
+                                       'District of Columbia': 'DC',
+                                       'Florida': 'FL',
+                                       'Georgia': 'GA',
+                                       'Hawaii': 'HI',
+                                       'Idaho': 'ID',
+                                       'Illinois': 'IL',
+                                       'Indiana': 'IN',
+                                       'Iowa': 'IA',
+                                       'Kansas': 'KS',
+                                       'Kentucky': 'KY',
+                                       'Louisiana': 'LA',
+                                       'Maine': 'ME',
+                                       'Maryland': 'MD',
+                                       'Massachusetts': 'MA',
+                                       'Michigan': 'MI',
+                                       'Minnesota': 'MN',
+                                       'Mississippi': 'MS',
+                                       'Missouri': 'MO',
+                                       'Montana': 'MT',
+                                       'Nebraska': 'NE',
+                                       'Nevada': 'NV',
+                                       'New Hampshire': 'NH',
+                                       'New Jersey': 'NJ',
+                                       'New Mexico': 'NM',
+                                       'New York': 'NY',
+                                       'North Carolina': 'NC',
+                                       'North Dakota': 'ND',
+                                       'Ohio': 'OH',
+                                       'Oklahoma': 'OK',
+                                       'Oregon': 'OR',
+                                       'Pennsylvania': 'PA',
+                                       'Rhodes Island': 'RI',
+                                       'South Carolina': 'SC',
+                                       'South Dakota': 'SD',
+                                       'Tennessee': 'TN',
+                                       'Texas': 'TX',
+                                       'Utah': 'UT',
+                                       'Vermont': 'VT',
+                                       'Virginia': 'VA',
+                                       'Washington': 'WA',
+                                       'West Virginia': 'WV',
+                                       'Wisconsin': 'WI',
+                                       'Wyoming': 'WY'})
 
-new_df =df.assign(Crime=1)
-
+new_df = df.assign(Crime=1)
 
 external_stylesheets = [
     {
@@ -113,19 +112,19 @@ app.layout = html.Div(
                     children=[
                         html.H3(children="Number crimes", className="header-title"),
                         html.P(children=nbr_crime)
-                    ], className="data1"
+                    ], className="data"
                 ),
                 html.Div(
                     children=[
                         html.H3(children="Most popular weapon", className="header-title"),
                         html.P(children=Weapon)
-                    ], className="data2"
+                    ], className="data"
                 ),
                 html.Div(
                     children=[
                         html.H3(children="Solved crimes", className="header-title"),
                         html.P(children=Mean + " % ")
-                    ], className="data3"
+                    ], className="data"
                 )
 
             ],
@@ -135,7 +134,7 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=[
-                        html.Div(children="Year", className="menu-title"),
+                        html.H2(children="Choose a year", className="menu-title"),
                         dcc.Dropdown(
                             id="year-filter",
                             options=OptionsListe,
@@ -144,28 +143,34 @@ app.layout = html.Div(
                             placeholder="Select a year",
 
                         ),
-                        html.P(
-                           children='most popular weapon'
+                        html.H3(
+                            children='Most popular weapon',
+                            className="texte"
 
                         ),
                         html.P(
-                           id="weapon"
+                            id="weapon",
+                            className="chiffre"
+
+                        ),
+                        html.H3(
+                            children='Number of crimes',
+                            className = "texte"
 
                         ),
                         html.P(
-                           children='Number of crimes'
+                            id="crime",
+                            className="chiffre"
+
+                        ),
+                        html.H3(
+                            children='Pourcentage of solved crimes',
+                            className="texte"
 
                         ),
                         html.P(
-                           id="crime"
-
-                        ),
-                        html.P(
-                           children='Pourcentage of solved crimes'
-
-                        ),
-                        html.P(
-                           id="Solved"
+                            id="Solved",
+                            className="chiffre"
 
                         ),
                     ],
@@ -186,9 +191,12 @@ app.layout = html.Div(
                 html.Div(
                     children=[
                         html.Div(
-                            children=dcc.Graph(
-                                id="fig2", config={"displayModeBar": False},
-                            ),
+                            children=
+
+                                dcc.Graph(
+                                id="fig2", config={"displayModeBar": False}),
+
+
 
                         )
 
@@ -202,16 +210,22 @@ app.layout = html.Div(
         html.Div(
             children=[
                 html.Div(
+                    className="vide"
+                ),
+                html.Div(
                     children=dcc.Graph(
                         id="map", config={"displayModeBar": False},
+                        className="mapp",
                     ),
 
+                ),
+                html.Div(
+                    className="vide"
                 )
 
             ],
             className="map_container"
         ),
-        
 
     ],
     className="container"
@@ -230,18 +244,29 @@ app.layout = html.Div(
 )
 def pie_chart(year):
     if year == "all":
+        print(1)
         fig = px.pie(df, values="Year", names='Victim_Sex')
+        fig.update_layout(
+            title = "Genders of the victims",
+            title_font_family="sans serif",
+            font=dict(
+                family="Courier New, monospace",
+                size=18,
+                color="black"
+            )
+               )
+
         fig2 = go.Figure()
         fig2.add_trace(go.Histogram(histfunc="count", x=df['Month']))
         weapon = df["Weapon"].value_counts().idxmax()
-        crime =df['Record_ID'].count()
+        crime = df['Record_ID'].count()
         Unsolved = len(df[df["Crime_Solved"] != "Yes"])
         solve = len(df[df["Crime_Solved"] == "Yes"])
         total = Unsolved + solve
         mean = (solve / total) * 100
         mean = round(mean, 2)
         Solved = str(mean)
-        print(1)
+        print(2)
         new_df1 = new_df.groupby(by=["State"]).sum()
         new_df1.reset_index(level=0, inplace=True)
         print(new_df1)
@@ -251,9 +276,11 @@ def pie_chart(year):
             locationmode='USA-states',
             locations=new_df1["State"],
             scope="usa",
+            color_continuous_scale="Reds",
             color='Crime',
+            title='Distribution of crimes in the USA',
             hover_data=['State', 'Crime'],
-            labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+
         )
 
     else:
@@ -280,12 +307,15 @@ def pie_chart(year):
             locationmode='USA-states',
             locations=new_df2["State"],
             scope="usa",
+            color_continuous_scale="Reds",
             color='Crime',
             hover_data=['State', 'Crime'],
-            labels={'Nb crimes': '% of Bee Colonies'},
+            range_color=(0, 3000),
+            title="Distribution of crimes by state, in the USA",
         )
 
     return fig, fig2, weapon, crime, Solved, map
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
